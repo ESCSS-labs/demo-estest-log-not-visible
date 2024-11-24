5619b8f71829858891f2a248fdce16bd6af133b8
mike <cspkno005@gmail.com>
2024-11-24 14:38:01 +0800

    refactor codebase

--- pages/index.vue
+++ pages/index.vue
@@ -63,15 +63,15 @@ const dataList = [
 <template>
   <main id="🌀Main">
     <h1 id="🌀Main__H1">ESCSS-ESTest Error Demo</h1>
-    <h2 id="🌀Main__H2">isVisible: false (<a id="🌀Main__H2__A" href="https://demo-estest-log-visible.netlify.app">for the true version link</a>); this is the default setting in production.
+    <h2 id="🌀Main__H2">Open DevTools (F12) to check error message.
+      <br>This is error message in production. (<a id="🌀Main__H2__A" href="https://demo-estest-log-visible.netlify.app">Check out in development</a>)
     </h2>
-    <h3 id="🌀Main__H3">Open DevTools (F12) to check error messages.</h3>
     <section id="🌀Main__X">
       <p id="🌀Main__X__P">
         type TYPES = 'undefined' | 'null' | 'array' | 'object' | 'boolean' | 'NaN' | 'number' | 'bigint' | 'string' |
         'symbol' | 'function'
       </p>
-      <p id="🌀Main__X__P2">declare function ESTest(input: any, type: TYPES, msg?: string): void</p>
+      <p id="🌀Main__X__P2">declare function ESTest(input: unknown, type: TYPES, errMsg?: string): void</p>
     </section>
 
     <section id="🌀Main__X2">
@@ -129,10 +129,6 @@ body {
 
 #🌀Main__H2 {
   @include bg-green-400;
-}
-
-#🌀Main__H3 {
-  @include bg-amber-400;
 }
 
 #🌀Main__H2__A {
